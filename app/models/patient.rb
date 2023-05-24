163527cbbc4347774612d2098bd1cbe444dfe686
annakynutsya <kunits.anya@gmail.com>
2023-05-25 00:48:46 +0300

    added responsive css

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -4,9 +4,7 @@ class Patient < ApplicationRecord
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  # attr_accessor :avatar
-  attr_accessor :admin
-  
+
   has_many :appointments
   has_many :doctors, through: :appointments
 
@@ -20,7 +18,6 @@ class Patient < ApplicationRecord
     false
   end
 
-  # use this instead of email_changed? for rails >= 5.1
   def will_save_change_to_email?
     false
   end
